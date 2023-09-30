4d292773280d508cfe07630dc5dfd74dea0c11ce
Ritesh Kumar Saw <108530662+imrish4@users.noreply.github.com>
2023-09-30 16:24:02 +0530

    Create evenOddNumber.java

---
+++ evenOddNumber.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class even_oddNum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number");
+        int num = sc.nextInt();
+        if(num%2==0)
+        System.out.println(num+" is even number");
+        else
+        System.out.println(num+" is odd number");
+    }
+}
